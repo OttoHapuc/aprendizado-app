af40b3b6ee92f249784741ee97cb7c16fe94bef3
OttoHapuc <dossantoshapuc@gmail.com>
2024-10-22 00:40:40 -0300

    feat: config components class based

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\View\Components\Atoms\Image;
+use App\View\Components\Atoms\InputSelect;
+use App\View\Components\Atoms\InputText;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Blade::component('image', Image::class);
+        Blade::component('input-select', InputSelect::class);
+        Blade::component('input-text', InputText::class);
     }
 }
